9e1ccfb670e7c610f633e3a6aaaf3b235b8d2b5d
MilkFather <31627231+MilkFather@users.noreply.github.com>
2021-08-30 20:17:59 +0800

    argument support

---
+++ qrcli/src/args.rs
@@ -0,0 +1,67 @@
+use clap::{App, Arg};
+
+pub fn build_argparse() -> App<'static, 'static> {
+	App::new("QR cli")
+		.about("Create QR code from command line")
+		.version(crate_version!())
+		.args(&[
+			Arg::with_name("INPUT")
+				.required(true)
+				.index(1)
+				.help("Sets the text to encode"),
+			Arg::with_name("enc")
+				.long("enc")
+				.required(true)
+				.value_name("ENC")
+				.takes_value(true)
+				.case_insensitive(true)
+				.possible_values(&["auto", "numeric", "alphanumeric", "byte", "kanji"])
+				.default_value("auto")
+				.help("Sets the preferred text encode mode"),
+			Arg::with_name("ec")
+				.long("ec")
+				.required(true)
+				.value_name("LVL")
+				.takes_value(true)
+				.case_insensitive(true)
+				.possible_values(&["L", "M", "Q", "H"])
+				.default_value("M")
+				.help("Sets the error correction level for encoding"),
+			Arg::with_name("ver")
+				.long("ver")
+				.required(true)
+				.value_name("VERSION")
+				.takes_value(true)
+				.default_value("auto")
+				.help("Sets the preferred QR code version, should be 1~40"),
+			Arg::with_name("imsize")
+				.long("imsize")
+				.value_name("SIZE")
+				.takes_value(true)
+				.required(true)
+				.conflicts_with("bitsize")
+				.help("Sets the total size (in pixels) in the output image"),
+			Arg::with_name("bitsize")
+				.long("bitsize")
+				.required(true)
+				.value_name("SIZE")
+				.takes_value(true)
+				.conflicts_with("imsize")
+				.help("Sets the size (in pixels) of individual bit in the QR code"),
+			Arg::with_name("silent")
+				.long("silent")
+				.required(true)
+				.value_name("SIZE")
+				.takes_value(true)
+				.default_value("4")
+				.help("Sets the size (in bits) of the silent zone around the QR code matrix"),
+			Arg::with_name("output")
+				.long("output")
+				.short("o")
+				.required(true)
+				.value_name("FILE")
+				.takes_value(true)
+				.default_value("output.png")
+				.help("Sets the output file path and name"),
+		])
+}
--- qrcli/src/main.rs
+++ qrcli/src/main.rs
@@ -1,12 +1,64 @@
 use qr::prelude::*;
 use qr::writer::prelude::*;
 
+#[macro_use]
+extern crate clap;
+
+mod args;
+use args::build_argparse;
+
 fn main() {
-	let qr = make_qr(&String::from("hello world"), None, ErrorCorrectionLevel::M, None);
+	let arg = build_argparse().get_matches();
+	/* Validate args */
+	let input = arg.value_of("INPUT").unwrap(); // This argument is required so we always have a value
+	let pre_enc_mode = match arg.value_of("enc").unwrap().to_lowercase().as_str() {
+		"numeric" => Some(EncodeMode::Numeric),
+		"alphanumeric" => Some(EncodeMode::Alphanumeric),
+		"byte" => Some(EncodeMode::Byte),
+		"kanji" => Some(EncodeMode::Kanji),
+		"auto" | _ => None,
+	};
+	let ec_level = match arg.value_of("ec").unwrap().to_uppercase().as_str() {
+		"L" => ErrorCorrectionLevel::L,
+		"M" => ErrorCorrectionLevel::M,
+		"Q" => ErrorCorrectionLevel::Q,
+		"H" => ErrorCorrectionLevel::H,
+		_ => { panic!("Invalid error correction level bypassed argument check"); }
+	};
+	let perf_ver = if arg.value_of("ver").unwrap() == "auto" {
+		None 
+	} else {
+		let raw = value_t!(arg, "ver", u8).unwrap_or_else(|e| {
+			println!("Invalid version");
+			e.exit();
+		});
+		Some(raw)
+	};
+	let qrsize = if arg.value_of("imsize").is_some() {
+		let raw = value_t!(arg, "imsize", u32).unwrap_or_else(|e| {
+			println!("Invalid imsize");
+			e.exit();
+		});
+		QrSizeConfig::TotalSize(raw)
+	} else {
+		let raw = value_t!(arg, "bitsize", u32).unwrap_or_else(|e| {
+			println!("Invalid bitsize");
+			e.exit();
+		});
+		QrSizeConfig::BitSize(raw)
+	};
+	let silentzonesize = value_t!(arg, "silent", u32).unwrap_or_else(|e| {
+		println!("Invalid silentzone");
+		e.exit();
+	});
+	let output = arg.value_of("output").unwrap(); // This argument has a default so we always have a value
+
+	/* Make QR code */
+	let qr = make_qr(&String::from(input), pre_enc_mode, ec_level, perf_ver);
 	match qr {
-		Err(s) => { println!("{}", s) },
+		Err(s) => { println!("Error, abort: {}", s) },
 		Ok(q) => {
-			q.to_image_luma(QrSizeConfig::BitSize(16), &[0], &[255], 4).save("hello.png").unwrap();
+			q.to_image_luma(qrsize, &[0], &[255], silentzonesize).save(output).unwrap();
 		}
 	}
 }
