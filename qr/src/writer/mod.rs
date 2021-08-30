a1cd4bbf900f0960abeb80f3c3ce74372c9d6eac
MilkFather <31627231+MilkFather@users.noreply.github.com>
2021-08-30 15:56:49 +0800

    Split into rust-qr

--- qr/src/writer/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod prelude;
-
-#[cfg(feature = "image")]
-mod image;
-
-#[cfg(feature = "json")]
-mod json;
--- qr/src/writer/prelude.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(feature = "image")]
-pub use crate::writer::image::{
-	QrSizeConfig,
-};
-
-#[cfg(feature = "json")]
-pub use crate::writer::json::*;
